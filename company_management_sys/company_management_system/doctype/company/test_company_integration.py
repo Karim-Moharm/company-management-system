7862b908e30fa232eacb9837fcc02e3e391dbb8f
Karim Moharm <karimmoharm222@gmail.com>
2025-12-19 14:40:20 +0200

    test: integration test for company

--- company_management_sys/company_management_system/doctype/company/test_company_integration.py
+++ company_management_sys/company_management_system/doctype/company/test_company_integration.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, Karim Moharm and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests import IntegrationTestCase
 
 
@@ -19,4 +19,48 @@ class IntegrationTestCompany(IntegrationTestCase):
 	Use this class for testing interactions between multiple components.
 	"""
 
-	pass
+	def test_company_with_fields(self):
+		"""
+		integration test where we create a company create
+		department, employee and projects linked to it
+		"""
+		company = frappe.get_doc({
+			"doctype": "Company",
+			"company_name": "test company"
+		}).insert()
+		# create department for that company
+		dept = frappe.get_doc({
+			"doctype": "Department",
+			"department_name": "marketing",
+			"company": company.name,
+		}).insert()
+		# create employee for that company
+		empl = frappe.get_doc({
+
+			"doctype": "Employee",
+			"employee_name":  "ahmed",
+			"email_address": "ahmed@me.com",
+			"mobile_number": "+201111111112",
+			"department": dept.name,
+			"company": company.name,
+		}).insert()
+		# create project for that company
+		project = frappe.get_doc({
+
+			"doctype": "Project",
+			"project_name":  "test project",
+			"description": "this is desc",
+			"start_date": "2025-12-14",
+			"department": dept.name,
+			"company": company.name,
+		}).insert()
+
+		company.reload()
+		self.assertEqual(company.number_of_departments, 1)
+		self.assertEqual(company.number_of_employees, 1)
+		self.assertEqual(company.number_of_projects, 1)
+
+		project.delete()
+		empl.delete()
+		dept.delete()
+		company.delete()
