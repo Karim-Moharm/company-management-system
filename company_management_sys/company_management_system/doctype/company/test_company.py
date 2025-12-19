b0780a3efc94d17cc05119bd7510f7dae56c3158
Karim Moharm <karimmoharm222@gmail.com>
2025-12-19 07:25:32 +0200

    test: unit test cases for company

--- company_management_sys/company_management_system/doctype/company/test_company.py
+++ company_management_sys/company_management_system/doctype/company/test_company.py
@@ -1,22 +1,45 @@
-# Copyright (c) 2025, Karim Moharm and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests import IntegrationTestCase
-
-
-# On IntegrationTestCase, the doctype test records and all
-# link-field test record dependencies are recursively loaded
-# Use these module variables to add/remove to/from that list
-EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
-IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+from frappe.tests.utils import FrappeTestCase
+import frappe
+from frappe.exceptions import MandatoryError
 
 
 
-class IntegrationTestCompany(IntegrationTestCase):
-	"""
-	Integration tests for Company.
-	Use this class for testing interactions between multiple components.
-	"""
+class TestCompany(FrappeTestCase):
+    """
+    unit test cases for company docType
+    """
+    def setUp(self):
+        self.company = frappe.get_doc({
+            "doctype": "Company",
+            "company_name": "microsoft" 
+        }).insert()
+    
+    def tearDown(self):
+        if self.company:
+            return self.company.delete()
+        
+    def test_create_company(self):
+        """
+        test case for creating a company
+        """
+        company = frappe.get_doc({
+            "doctype": "Company",
+            "company_name": "test company"
+        }).insert()
+        self.assertEqual(company.company_name, "test company")
+        self.assertEqual(company.number_of_departments, 0)
+        self.assertEqual(company.number_of_employees, 0)
+        self.assertEqual(company.number_of_pojects, "test company")
 
-	pass
+        company.delete()
+
+
+    def test_requied_company_name(self):
+        """test case for a required field"""
+        with self.assertRaises(MandatoryError):
+            frappe.get_doc({
+                "doctype": "Company"
+            }).insert()
+
+
+    
